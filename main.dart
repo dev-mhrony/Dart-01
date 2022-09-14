2338173981b7baa070d23e84bed00ec1d4364196
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-09-14 12:23:41 +0600

    Last update - 14/09/2022

--- main.dart
+++ main.dart
@@ -1,14 +1,25 @@
 void main() {
+  // String
   String name = 'MH RONY';
+
+  // Intager
   int age = 22;
+
+  // Array / List
   List address = ["Sonatola", "Bogura"];
+
+  // Map/ create a Indivisual index name
   Map about = {
     "name": "MH RONY",
     "nic_name": "Dhanvi",
     "age": 22,
     "address": "Sonatola, Bogura."
   };
+
+  // double / fraction number
   double info = 1.14;
+
+  // Use Month name in list data type
   List monthName = [
     "January",
     "February",
@@ -24,12 +35,15 @@ void main() {
     "December"
   ];
 
+// All Dataa Show the "Print("Text") Function"
   print("$name");
   print("$age");
   print("$address");
   print("$about");
   print("$info");
   print("$monthName");
+
+  // Show Data type in use variable => print(variable.runtimeType); >>>>> use: "runtimeType"
   print(name.runtimeType);
   print(age.runtimeType);
   print(address.runtimeType);
